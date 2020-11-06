dbf185e6a0a28ca5292a880236daef06482b325d
fox-yokai <pat.w.cain@gmail.com>
2020-11-05 20:52:26 -0600

    cleaned up comments

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 $(document).ready(function() {
 
-    // Make our variables global to the runtime of our application
     var firstNumber = 0;
     var secondNumber = 0;
     var operator = "";
@@ -8,8 +7,6 @@ $(document).ready(function() {
     var isOperatorChosen = false;
     var isCalculated = false;
 
-    // Use a function to initialize our calculator.
-    // This way when the user hits clear, we can guarantee a reset of the app.
     function initializeCalculator() {
       firstNumber = "";
       secondNumber = "";
@@ -22,12 +19,10 @@ $(document).ready(function() {
 
     $(".number").click(function() {
 
-      // Check if we've already run a calculation, if so... we'll just.
       if (isCalculated) {
         return false;
       }
 
-      // If operator is chosen, we should be writing the secondNumber, otherwise, the firstNumber
       if (isOperatorChosen) {
         secondNumber += $(this).val();
         $("#second-number").text(secondNumber);
@@ -41,38 +36,26 @@ $(document).ready(function() {
     });
     $(".operator").on("click", function() {
 
-      // Check if a first number has already been selected
-      // Or we've already run a calculation, if so we just exit.
       if (!firstNumber || isCalculated) {
         return false;
       }
 
-      // Set isOperatorChosen to true so we start writing to secondNumber
       isOperatorChosen = true;
-
-      // Store off the operator
       operator = $(this).val();
 
-      // Set the HTML of the #operator to the text of what was clicked
       $("#operator").text($(this).text());
 
     });
     $(".equal").on("click", function() {
-
-      // If we already clicked equal, don't do the calculation again
       if (isCalculated) {
         return false;
       }
 
-      // Set isCalculated to true so that we don't get in a weird UI state by clicking buttons again
       isCalculated = true;
 
-      // Use parseInt to convert our string representation of numbers into actual integers
       firstNumber = parseInt(firstNumber);
       secondNumber = parseInt(secondNumber);
 
-      // Based on the operator that was chosen.
-      // Then run the operation and set the HTML of the result of that operation
       if (operator === "plus") {
         result = firstNumber + secondNumber;
       }
@@ -97,12 +80,8 @@ $(document).ready(function() {
 
     });
     $(".clear").on("click", function() {
-
-      // Call initializeCalculater so we can reset the state of our app
       initializeCalculator();
-
     });
 
-    // Call initializeCalculater so we can set the state of our app
     initializeCalculator();
   });
